5338ea34771683a40f395ceba9355bce46fca65e
Florence Ndemi <ndemiflorence@gmail.com>
2025-06-26 00:02:51 +0300

    Commit:first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './pages/**/*.html',
+    './components/**/*.html',
+    './js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        goldenrod: '#daa520',
+        greenCustom: '#016f32',
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: 'class', 
+  plugins: [],
+}
